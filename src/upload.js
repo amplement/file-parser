aa08274bfc048ce4643230c1b7db9bf67d88d69d
Cédric <cedricbale@amplement.com>
2018-09-11 11:37:45 +0200

    try bucket storage

--- src/upload.js
+++ src/upload.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fs from 'fs';
 import multer from 'multer';
 import csv from 'fast-csv';
-import path from 'path';
+// import path from 'path';
 import uuid from 'uuid/v4';
 
 const EMAIL_REGEX = /([a-zA-Z0-9._+-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
@@ -11,10 +11,10 @@ const noop = () => {};
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
 
-        const __root = path.join(__dirname, '../');
-        const tmpPath = path.join(__root, './tmp/');
+        // const __root = path.join(__dirname, '../');
+        // const tmpPath = path.join(__root, './tmp/');
 
-        callback(null, tmpPath);
+        callback(null, 'gs://file-parser-216107.appspot.com/');
 
     },
     filename: (req, file, callback) => {
